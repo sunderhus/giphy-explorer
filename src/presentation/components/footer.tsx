5cecd6fc1720d58c5c562a863928ab901fffb725
sunderhus <matheus.sunderhus@gmail.com>
2024-02-24 16:18:25 -0300

    feat(presentation): extract footer component to its own file

---
+++ src/presentation/components/footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="flex  mt-auto text-slate-300">
+      <p>
+        Code challenge <span className="text-blue-500"> - BlueCoding</span>
+        <span className="text-green-400"> - Matheus </span>
+        <span className="text-green-400">Sunderhus</span>
+      </p>
+    </footer>
+  );
+}
