0c45515167ecfeeae7c28cabc53be87684c24239
sunderhus <matheus.sunderhus@gmail.com>
2024-02-24 17:05:49 -0300

    styles(presentation): add Giphy colors to borders in PageTemplate

--- src/presentation/templates/page-template.tsx
+++ src/presentation/templates/page-template.tsx
@@ -1,7 +1,8 @@
 export function PageTemplate({ children }: { children: React.ReactNode }) {
   return (
-    <main className="grid grid-rows-[auto_1fr_auto] min-h-screen gap-4 p-4 ">
+    <main className="flex flex-col min-h-screen gap-4 p-4 mx-auto w-xl border-r-violet-400 border-8 border-b-blue-400 border-t-yellow-400 border-l-green-400">
       {children}
+      <div className="bg-red-500 w-14 h-24 fixed top-0 right-0 -translate-y-1/2 hidden sm:block rounded" />
     </main>
   );
 }
