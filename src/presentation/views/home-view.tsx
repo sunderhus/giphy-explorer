6368ad5207bfd62dc766273a6449d4dc1f27dd6a
sunderhus <matheus.sunderhus@gmail.com>
2024-02-25 12:52:42 -0300

    feat(presentation): create grid template for giphy gallery and add it to home view

--- src/presentation/views/home-view.tsx
+++ src/presentation/views/home-view.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, FormEvent, useId, useState } from "react";
 import { Footer } from "@/presentation/components/footer";
 import { Header } from "@/presentation/components/header";
 import { SearchGiphysUseCase } from "@/domain/use-cases/SearchGiphys";
+import { Giphy } from "@/domain/models/Giphy";
 
 interface HomeProps {
   searchGiphys: SearchGiphysUseCase;
@@ -11,14 +12,36 @@ interface HomeProps {
 export function HomeView({ searchGiphys }: HomeProps) {
   const searchInputId = useId();
   const [searchText, setSearchText] = useState("");
+  const [offSet, setOffset] = useState(0);
+  const [giphys, setGiphys] = useState<Giphy[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const hasGiphysToShow = giphys.length > 0;
 
   const onSearch = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const result = await searchGiphys.search(searchText, 10);
-      console.log(result);
+      setIsLoading(true);
+      const results = await searchGiphys.search(searchText, 0);
+      setGiphys([...results]);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleLoadMore = async () => {
+    try {
+      setIsLoading(true);
+      const nextOffSet = offSet + 1;
+      const results = await searchGiphys.search(searchText, nextOffSet);
+      setOffset(nextOffSet);
+      setGiphys((state) => [...results, ...state]);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -58,12 +81,48 @@ export function HomeView({ searchGiphys }: HomeProps) {
             />
           </div>
           <button
-            className="bg-violet-400 text-violet-950 font-semibold hover:ring-2 hover:ring-purple-900 outline-none max-lg:w-full h-12 w-32 "
+            className="bg-violet-400 text-violet-950 font-semibold hover:ring-2 hover:ring-purple-900 outline-none max-lg:w-full h-12 w-32 p-3 text-center"
             type="submit"
+            disabled={isLoading}
           >
-            Search
+            {isLoading ? (
+              <div className="flex flex-1 items-center justify-center gap-3">
+                <span className="relative flex h-3 w-3">
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-violet-900 opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-3 w-3 bg-violet-500"></span>
+                </span>
+                Loading
+              </div>
+            ) : (
+              "Search"
+            )}
           </button>
         </form>
+      </section>
+
+      {!hasGiphysToShow && (
+        <p className="text-gray-100">waiting for you awesome search</p>
+      )}
+
+      <section>
+        <button
+          type="button"
+          disabled={isLoading}
+          className="text-gray-100 p-4 bg-yellow-400 mb-4"
+          onClick={handleLoadMore}
+        >
+          LoadMore
+        </button>
+        <div className="grid grid-cols-gallery gap-3">
+          {giphys.map((giphy) => (
+            <img
+              className="aspect-video w-full h-48 object-cover rounded-s-sm rounded-e-3xl"
+              key={giphy.url}
+              src={giphy.url}
+              alt="giphy image"
+            />
+          ))}
+        </div>
       </section>
 
       <Footer />
